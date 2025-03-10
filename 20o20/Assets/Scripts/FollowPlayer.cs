42bd90e0aded3a4823f0c2d26dcbb40ff6209e6d
ZeAntonioM <joseapmartins2003@gmail.com>
2025-03-10 18:17:34 +0000

    Fixed interface

--- 20o20/Assets/Scripts/FollowPlayer.cs
+++ 20o20/Assets/Scripts/FollowPlayer.cs
@@ -1,28 +1,27 @@
+using System;
 using UnityEngine;
 
-public class followPlayer : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
     [SerializeField] private Transform player;
+
     [Header("Transition Control")]
     [SerializeField] private float smoothTime = 0.3f;
     [SerializeField] private Vector3 velocity;
 
-    private double lastY;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float yoffset = 0.0f;
+
     void Start()
     {
-        lastY = player.position.y;
+        yoffset = transform.position.y - player.position.y;
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        if (player.position.y > lastY)
-        {
-            Vector3 newPos = new Vector3(transform.position.x, player.position.y, transform.position.z);
-            transform.position = Vector3.SmoothDamp(transform.position, newPos, ref velocity, smoothTime);
-            lastY = player.position.y;
-        }
+        Vector3 targetPosition = new Vector3(player.position.x, player.position.y + yoffset, transform.position.z);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
     }
 }
